51168c8d207dafdcf67a962a594aa359f838dd10
Aaron Passchier <aaron.passchier@bissell.com>
2019-12-31 14:33:40 -0500

    Added NeoSelect.vue component

---
+++ components/neo-material/NeoSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="onClick">
+    <span>
+      <span>
+        <slot/>
+      </span>
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NeoSelect extends Vue {
+    onClick() {
+
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- pages/neo-material/index.vue
+++ pages/neo-material/index.vue
@@ -1,22 +1,32 @@
 <template>
-  <section class="container">
+  <section class="container flex flex-col">
     <NeoButton>A Button</NeoButton>
+    <NeoSelect>A Select</NeoSelect>
   </section>
 </template>
 
 <script lang="ts">
   import {Vue, Component} from 'vue-property-decorator';
   import NeoButton from "~/components/neo-material/NeoButton.vue";
+  import NeoSelect from "~/components/neo-material/NeoSelect.vue";
 
   @Component({
-    components: {NeoButton}
+    components: {NeoSelect, NeoButton}
   })
   export default class NeoMaterialIndexPage extends Vue {
   }
 </script>
 
 <style scoped lang="scss">
-.container {
-  margin: 2rem;
-}
+  .container {
+    margin: 2rem;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  .flex-col {
+    flex-direction: column;
+  }
 </style>
