d1d4ba72f4f190546daf088116d886f37a1449e6
Aaron Passchier <aaron.passchier@bissell.com>
2019-12-31 11:55:02 -0500

    Added neo material page and NeoButton.vue component

---
+++ components/neo-material/NeoButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NeoButton extends Vue {
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ pages/neo-material/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <NeoButton>A Button</NeoButton>
+  </section>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator';
+  import NeoButton from "~/components/neo-material/NeoButton.vue";
+  @Component({
+    components: {NeoButton}
+  })
+  export default class NeoMaterialIndexPage extends Vue {
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
